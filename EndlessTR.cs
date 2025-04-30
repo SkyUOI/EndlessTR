23c0ccf25145064a587c53f5fb9a3db7137c9d67
limuy <limuyang202011@163.com>
2025-04-30 21:28:02 +0800

    add .gitignore

--- EndlessTR.cs
+++ EndlessTR.cs
@@ -10,6 +10,14 @@ namespace EndlessTR
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class EndlessTR : Mod
 	{
+		public override void Load()
+		{
+			base.Load();
+		}
 
+		public override void Unload()
+		{
+			base.Unload();
+		}
 	}
 }
