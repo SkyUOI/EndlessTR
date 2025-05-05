3c67503f3f429728dae5a98ca50a7f577cb77173
limuy2022 <limuy202011@163.com>
2025-05-05 21:52:06 +0800

    Fix Evil Generation

---
+++ Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+
+namespace EndlessTR
+{
+    internal class Utils
+    {
+        public static bool IsCrimsonHeart(Tile block)
+        {
+            if (block.TileType == TileID.ShadowOrbs)
+            {
+                return (block.TileFrameX >= 36);
+            }
+            return false;
+        }
+    }
+}
